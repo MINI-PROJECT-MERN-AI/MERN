78ee82fca5bbb7b7b3de3a32350e79db1d592533
Abhishek TG <153442145+Abhishek-TG18@users.noreply.github.com>
2024-11-25 14:09:46 +0530

    Updated HomePage2

--- src/routes/Homepage/Homepage.jsx
+++ src/routes/Homepage/Homepage.jsx
@@ -1,78 +1,4 @@
-// import { Link } from 'react-router-dom'
-// import './Homepage.css'
 
-// const Homepage = () => {
-//   return (
-//     <div className='homepage'>
-
-//       <div className="left">
-//         <h1>MERN AI</h1>
-//         <h2>Supercharge Communication with AI Power</h2>
-//         <h3>MERN AI is a smart chat application designed to provide users with engaging, real-time conversational experiences, offering accurate and context-aware responses for seamless communication.</h3>
-//         <Link to="/dashboard">Get Started</Link>
-//       </div>
-//       <div className="right">
-//         <div className="imgContainer">
-//           <div className="bgContainer">
-//             <div className="bg"></div>
-//           </div>
-//           <img src="/bot.png" alt="bot" className='bot' />
-//           <div className="chat">
-//             <img
-//               src={
-//                 typingStatus === "human1"
-//                   ? "/human1.jpeg"
-//                   : typingStatus === "human2"
-//                     ? "/human2.jpeg"
-//                     : "bot.png"
-//               }
-//               alt=""
-//             />
-//             <TypeAnimation
-//               sequence={[
-//                 // Same substring at the start will only be typed out once, initially
-//                 "Human:We produce food for Mice",
-//                 2000,
-//                 () => {
-//                   setTypingStatus("bot");
-//                 },
-//                 "Bot:We produce food for Hamsters",
-//                 2000,
-//                 () => {
-//                   setTypingStatus("human2");
-//                 },
-//                 "Human2:We produce food for Guinea Pigs",
-//                 2000,
-//                 () => {
-//                   setTypingStatus("bot");
-//                 },
-//                 "Bot:We produce food for Chinchillas",
-//                 2000,
-//                 () => {
-//                   setTypingStatus("human1");
-//                 },
-//               ]}
-//               wrapper="span"
-//               repeat={Infinity}
-//               cursor={true}
-//               omitDeletionAnimation={true}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//       <div className="terms">
-//         <img src="/logo.png" alt="" />
-//         <div className="links">
-//           <Link to="/">Terms of Service</Link>
-//           <span>|</span>
-//           <Link to="/">Privacy Policy</Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Homepage;
 
 import { Link } from "react-router-dom";
 import "./Homepage.css";
@@ -112,13 +38,13 @@ const Homepage = () => {
             />
             <TypeAnimation
               sequence={[
-                // Same substring at the start will only be typed out once, initially
-                "Human:We produce food for Mice",
+                
+                "Human: What can this AI chat application do?",
                 2000,
                 () => {
                   setTypingStatus("bot");
                 },
-                "Bot:We produce food for Hamsters",
+               "Bot: It provides real-time, intelligent, and context-aware conversations.",
                 2000,
                 () => {
                   setTypingStatus("human2");
